a4f3a6eb4032bae13108c5c784a697c7cd4052d5
Mr. Roxanne <64316235+mr-roxanne@users.noreply.github.com>
2020-06-07 01:33:05 -0300

    make it better

--- website/base.py
+++ website/base.py
@@ -11,5 +11,5 @@ class Server(Quart):
 
 
   def run(self):
-  	  port = int(os.environ.get('PORT', 3000))
+      port = int(os.environ.get('PORT', 3000))
       super().run(host='0.0.0.0', port=port)
