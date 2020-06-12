33fad0a104e1e0088b8b4a6602fbfdf9b8de4938
Mr. Roxanne <64316235+mr-roxanne@users.noreply.github.com>
2020-06-12 01:08:09 -0300

    make it better

--- website/route/index.py
+++ website/route/index.py
@@ -8,10 +8,17 @@ class Index(Quart):
         self.bot = bot
         self.route = Blueprint('index', __name__, url_prefix="/")
         self.route.add_url_rule("/", 'index', self.index)
-        self.route.add_url_rule("/index", 'index', self.index)
+        self.route.add_url_rule("index", 'index', self.index)
+        self.route.add_url_rule("guilds", 'indexs', self.guilds)
+        self.route.add_url_rule("cmds", 'indexz', self.cmds)
 
     async def index(self):
         guilds = [g for g in self.bot.guilds]
         data = [len(guilds), sum(g.member_count for g in guilds if not g.unavailable)]
         return await render_template('index.html', data=data)
 
+    async def guilds(self):
+        return await render_template('guilds.html')
+
+    async def cmds(self):
+        return await render_template('cmds.html')        
