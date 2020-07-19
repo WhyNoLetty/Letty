6c4f2e9cb0bf1ef73a48e06a9e5dbb78ecaffc0d
Mr. Roxanne <64316235+mr-roxanne@users.noreply.github.com>
2020-07-19 00:12:19 -0300

    Versão (0.2.0)
    Começar o rewrite.
    • inciando um 'rework' no projeto.

--- database/__init__.py
+++
@@ -1,8 +0,0 @@
-#import's necessários
-from .base import on_connect_db
-
-"""
-- Observação:
- O Arquivo __init__.py vai ser meio que uma importação 'global'
- de todo os modulos e configuração da pasta DATABASE E suas Conexões.
-"""
--- database/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#import's necessários
-from .guild import Guild
-
-"""
-- Observação:
- O Arquivo __init__.py vai ser meio que uma importação 'global'
- de todo os modulos e configuração da pasta Models.
-"""
